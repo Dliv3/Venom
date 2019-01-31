dfec1afdbdce47882951acc39328ce5a230be743
Dliv3 <dddliv3@gmail.com>
2019-01-31 17:55:54 +0800

    Updated admin built-in

--- admin/cli/cli.go
+++ admin/cli/cli.go
@@ -91,9 +91,9 @@ func ShowUsage() {
   connect    [rhost] [rport]               Connect to a new node through current node.
   sshconnect [user@ip:port] [dport]        Connect to a new node through ssh tunnel.
   shell                                    Start an interactive shell on the target node.
-  upload     [local_file]  [remote_file]   Upload file to the target node.
-  download   [remote_file]  [local_file]   Download file from the target node.
-  socks      [lport]                       Start a socks server.
+  upload     [local_file]  [remote_file]   Upload files to the target node.
+  download   [remote_file]  [local_file]   Download files from the target node.
+  socks      [lport]                       Start a socks5 server.
   lforward   [lhost] [sport] [dport]       Forward a local sport to a remote dport.
   rforward   [rhost] [sport] [dport]       Forward a remote sport to a local dport.
 `)
